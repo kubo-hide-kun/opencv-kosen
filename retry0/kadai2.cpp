80ac8169a2b98ae6249c38ecb20314cdaf5e0cef
k16083hk <k16083hk@IIE33>
2019-12-23 14:40:32 +0900

    課題2の実相

--- retry0/kadai2.cpp
+++ retry0/kadai2.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 using namespace cv;
 
-int main1() {
+int main() {
 	Mat colorImage;
 	colorImage = imread("E:\\opencv320\\data\\mouse.jpg");
 	if (colorImage.empty()) {
 		printf("failed load image");
 		return -1;
 	}
-	imshow("Display Window", colorImage);
-	waitKey(0);
+	
 	Mat binaryImage;
 	cvtColor(colorImage, binaryImage, COLOR_BGR2GRAY);
 	imshow("Display Window", binaryImage);
+	waitKey(0);
+
+	Mat ResizedImage;
+	resize(binaryImage, ResizedImage, Size(), 0.5, 0.5, 0);
+	imshow("Display Window", ResizedImage);
 	waitKey(0);
 	return 0;
 }
