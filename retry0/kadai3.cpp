055ce13716582b881942d2ec47e7f0634dd742fc
k16083hk <k16083hk@IIE33>
2019-12-23 14:53:13 +0900

    課題3完了

--- retry0/kadai3.cpp
+++ retry0/kadai3.cpp
@@ -2,22 +2,24 @@
 using namespace std;
 using namespace cv;
 
-int main2() {
+int main() {
 	Mat colorImage;
 	colorImage = imread("E:\\opencv320\\data\\mouse.jpg");
 	if (colorImage.empty()) {
 		printf("failed load image");
 		return -1;
 	}
-	
+
 	Mat binaryImage;
 	cvtColor(colorImage, binaryImage, COLOR_BGR2GRAY);
 	imshow("Display Window", binaryImage);
 	waitKey(0);
 
-	Mat ResizedImage;
-	resize(binaryImage, ResizedImage, Size(), 0.5, 0.5, 0);
-	imshow("Display Window", ResizedImage);
+	Mat AffinedImage;
+	Point2d ctr(binaryImage.cols / 2, binaryImage.rows / 2);
+	Mat mat = getRotationMatrix2D(ctr, 45, 1);
+	warpAffine(binaryImage, AffinedImage, mat, AffinedImage.size(), CV_INTER_LINEAR, cv::BORDER_TRANSPARENT);
+	imshow("Display Window", AffinedImage);
 	waitKey(0);
 	return 0;
 }
